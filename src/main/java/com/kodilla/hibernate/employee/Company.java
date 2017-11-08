1aec1f1e5f3d138fb8aebdf7081d23c3662b8b2b
izagaluszewska <izabellagaluszewska@wp.pl>
2017-11-08 23:33:39 +0100

    zadanie 13.4

---
+++ src/main/java/com/kodilla/hibernate/employee/Company.java
@@ -0,0 +1,59 @@
+package com.kodilla.hibernate.employee;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@NamedNativeQuery(
+        name = "Company.findCompaniesByNameFirstThreeLettersLike",
+        query = "SELECT * FROM COMPANIES" +
+                " WHERE SUBSTRING(COMPANY_NAME, 1, 3) LIKE :NAME",
+        resultClass = Company.class
+)
+
+@Entity
+@Table(name = "COMPANIES")
+public class Company {
+    private int id;
+    private String name;
+    private List<Employee> employees = new ArrayList<>();
+
+    public Company() {
+    }
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "COMPANY_ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Column(name = "COMPANY_NAME")
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "companies")
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    private void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
